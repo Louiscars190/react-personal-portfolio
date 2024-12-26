bed1a7194e6ef95026aca30c5cbbf0102f7ff324
louiscars190 <louiscars170@gmail.com>
2024-12-26 14:32:40 -0500

    downgraded react to 18.2 because of dependencies

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,13 +1,16 @@
-"use client";
-
 export default function Home() {
   return (
     <div className="font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-4 min-h-screen justify-center items-center">
-        <div className="text-7xl">
-          Welcome!
+        <div className="flex flex-row gap-72">
+          <div className="text-7xl">
+            Welcome!
+          </div>
+          <div className="text-2xl">
+          stuff rninggnignrngiirnignirgnrini
+          </div>
         </div>
       </main>
     </div>
-  );
+  )
 }
